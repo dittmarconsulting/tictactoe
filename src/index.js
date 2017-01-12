d158e904411c20dc6656c233314393b3e8787e52
Thomas Dittmar <dittmarconsulting@gmail.com>
2017-01-13 10:48:22 +1100

    Abstracted the store out of index.js into store.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware, compose} from 'redux'
 import {Provider} from 'react-redux'
-import logger from 'redux-logger'
-import thunk from 'redux-thunk'
-import rootReducer from './rootReducer'
+
+import store from './store'
 import GameContainer from './containers/gameContainer.js';
 import './static/css/index.css';
-
-const middleware = applyMiddleware(
-    thunk,
-    logger()
-)
-
-const store = createStore(
-    rootReducer,
-    compose(
-        middleware,
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-)
 
 ReactDOM.render(
     <Provider store={store}>
