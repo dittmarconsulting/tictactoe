2a847dc66da07ffcbb5bc19179c049c09fb20362
Thomas Dittmar <dittmarconsulting@gmail.com>
2017-01-13 10:33:29 +1100

    Header can be a simple function instead of a class

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import logo from '../static/img/logo.svg'
 
-export default class Header extends React.Component {
-    render() {
-        return (
-            <div className="gc-header">
-                <img src={logo} className="gc-logo" alt="logo"/>
-                <h2>TicTacToe with React & Redux</h2>
-            </div>
-        )
-    }
+const Header = () => {
+    return (
+        <div className="gc-header">
+            <img src={logo} className="gc-logo" alt="logo"/>
+            <h2>TicTacToe with React & Redux</h2>
+        </div>
+    )
 }
+
+export default Header
