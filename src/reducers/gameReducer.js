0b71859438ef3df03922eb3ff89616e61528479a
Thomas Dittmar <dittmarconsulting@gmail.com>
2017-01-12 14:40:47 +1100

    Swapped action strings with constants. Made sure all reducers are pure functions.

--- src/reducers/gameReducer.js
+++ src/reducers/gameReducer.js
@@ -1,4 +1,5 @@
 import cells from '../data/cells'
+import C from '../constants'
 
 const initState = {
     playerASymbol: 'X',
@@ -13,7 +14,8 @@ const initState = {
 export default function game(state=initState, action) {
     switch(action.type) {
 
-        case 'CELL_CLICKED' : {
+        // in case someone of a cell click action call
+        case C.CELL_CLICKED : {
             return {
                 ...state,
                 cells: [
@@ -27,66 +29,48 @@ export default function game(state=initState, action) {
             }
         }
 
-        case 'INC_MOVE' : {
+        // in case of a move increment action call
+        case C.INC_MOVE : {
             return {
                 ...state,
-                currentMove: state.currentMove + 1
+                currentMove: action.payload
             }
         }
 
-
-        case 'PLAYER_CHANGED' : {
+        // in case of a swap player action call
+        case C.SWAP_PLAYER : {
             return {
                 ...state,
-                currentPlayer: (state.currentPlayer === state.playerASymbol) ? state.playerBSymbol : state.playerASymbol
+                currentPlayer: action.payload
             }
         }
 
-        case 'CHECK_WINNER' : {
-            // define winning combinations
-            const winningCellIds = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
-            // search for the winning cell combination (i.e. [2,5,8])
-            // it returns the found array or undefined
-            const check = winningCellIds.find((cellIds) => {
-                // init return value
-                let retVal = false
-                // store the text of the 3 board cells of the winning combination (i.e. [2,5,8]) in vars
-                let firstCell = state.cells[cellIds[0]].text
-                let secondCell = state.cells[cellIds[1]].text
-                let thirdCell = state.cells[cellIds[2]].text
-                // check for null
-                if (firstCell && secondCell && thirdCell) {
-                    // check if cells of the board have the same symbols
-                    // (i.e.  2     5  &&  5     8 )
-                    // (i.e. 'X'==='X' && 'X'==='X')
-                    if (firstCell === secondCell && secondCell === thirdCell) {
-                        // we have a match
-                        retVal = true
-                    }
-                }
-                return retVal
-            })
+        // in case of a check winner action call
+        case C.CHECK_WINNER : {
             return {
                 ...state,
-                winnerFound: (check !== undefined) // convert return value into a truthy value
+                winnerFound: action.payload
             }
         }
 
-        case 'OPEN_DIALOG' : {
+        // in case of an open dialog action call
+        case C.OPEN_DIALOG : {
             return {
                 ...state,
                 showDialog: true
             }
         }
 
-        case 'CLOSE_DIALOG' : {
+        // in case of a close dialog action call
+        case C.CLOSE_DIALOG : {
             return {
                 ...state,
                 showDialog: false
             }
         }
 
-        case 'RESET_GAME' : {
+        // in case of a reset game action call
+        case C.RESET_GAME : {
             return {
                 ...state,
                 cells: cells,
