4a64d5a796fac41821aee50a1a02ec1bc2462ca5
Thomas Dittmar <dittmarconsulting@gmail.com>
2017-01-13 10:59:50 +1100

    Abstracted the initState out of gameReducers.js into a separate file

---
+++ src/initState.js
@@ -0,0 +1,22 @@
+const initState = {
+    cells: [
+        {id: 0,text: null},
+        {id: 1,text: null},
+        {id: 2,text: null},
+        {id: 3,text: null},
+        {id: 4,text: null},
+        {id: 5,text: null},
+        {id: 6,text: null},
+        {id: 7,text: null},
+        {id: 8,text: null}
+    ],
+    playerASymbol: 'X',
+    playerBSymbol: 'O',
+    currentPlayer: 'X',
+    currentMove: 0,
+    winnerFound: false,
+    showDialog: false,
+    dialogMessage: null
+}
+
+export default initState
