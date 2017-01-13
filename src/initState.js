a5535c64a4c4a8f922b0b2dab7722f186009d5ba
Thomas Dittmar <dittmarconsulting@gmail.com>
2017-01-13 13:27:40 +1100

    dividing init state into two domains - gameState and dialogState

--- src/initState.js
+++ src/initState.js
@@ -1,22 +1,26 @@
 const initState = {
-    cells: [
-        {id: 0,text: null},
-        {id: 1,text: null},
-        {id: 2,text: null},
-        {id: 3,text: null},
-        {id: 4,text: null},
-        {id: 5,text: null},
-        {id: 6,text: null},
-        {id: 7,text: null},
-        {id: 8,text: null}
-    ],
-    playerASymbol: 'X',
-    playerBSymbol: 'O',
-    currentPlayer: 'X',
-    currentMove: 0,
-    winnerFound: false,
-    showDialog: false,
-    dialogMessage: null
+    gameState: {
+        cells: [
+            {id: 0,text: null},
+            {id: 1,text: null},
+            {id: 2,text: null},
+            {id: 3,text: null},
+            {id: 4,text: null},
+            {id: 5,text: null},
+            {id: 6,text: null},
+            {id: 7,text: null},
+            {id: 8,text: null}
+        ],
+        playerASymbol: 'X',
+        playerBSymbol: 'O',
+        currentPlayer: 'X',
+        currentMove: 0,
+        winnerFound: false
+    },
+    dialogState: {
+        showDialog: false,
+        dialogMessage: null
+    }
 }
 
 export default initState
