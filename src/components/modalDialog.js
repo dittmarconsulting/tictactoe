a4acdb0fc28c38b63f7dda8d30b0d5b7d927df44
Thomas Dittmar <dittmarconsulting@gmail.com>
2017-01-13 19:25:42 +1100

    removed console logging; removed the winner text because we now change the text with the dialogAction creator

--- src/components/modalDialog.js
+++ src/components/modalDialog.js
@@ -11,29 +11,20 @@ const customStyles = {
         marginRight           : '-50%',
         transform             : 'translate(-50%, -50%)'
     }
-};
+}
 
 Modal.defaultStyles.overlay.backgroundColor = 'rgba(0, 0, 0, 0.8)';
 
-
 const ModalDialog = (props) => {
-    const {showDialog, currentPlayer, winnerFound} = props.game
+    const {showDialog, dialogMessage} = props.dialog
     const {closeDialogAction} = props
-    // assign an empty winner text
-    let winnerText = null
-    // populate winner text based on state conditions
-    if(winnerFound) {
-        winnerText = 'Player `' + currentPlayer + '` won! Congratulation ...'
-    } else {
-        winnerText = 'It seems you reached an impasse!'
-    }
     return (
         <Modal
             isOpen={showDialog}
             onRequestClose={closeDialogAction}
             style={customStyles}
             contentLabel='Winner Modal'>
-            <h2>{winnerText}</h2>
+            <h2>{dialogMessage}</h2>
             <Button className="resetbtn" bsStyle="primary" onClick={closeDialogAction}> Play Again ? </Button>
         </Modal>
     )
