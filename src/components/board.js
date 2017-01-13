5385738ba007892aec449d54dcc8a8d63a251864
Thomas Dittmar <dittmarconsulting@gmail.com>
2017-01-13 19:45:22 +1100

    Refactoring

--- src/components/board.js
+++ src/components/board.js
@@ -1,31 +1,21 @@
 import React from 'react'
 
-export default class Board extends React.Component {
-
-    constructor() {
-        super()
-        this.cellComponent = this.cellComponent.bind(this)
-        this.cellClicked = this.cellClicked.bind(this)
-    }
-
-    cellClicked(id, text) {
-        // check if not null
-        if(!text)
-            this.props.cellClickAction(id)
-    }
-
-    cellComponent(cell) {
-        return (
-            <div key={cell.id} onClick={() => this.cellClicked(cell.id, cell.text)} className="cell">{cell.text}</div>
-        )
-    }
-
-    render() {
-        const {cells} = this.props.game
-        return (
-            <div className="board">
-                {cells.map(this.cellComponent)}
-            </div>
-        )
-    }
+const Board = (props) => {
+    const {cellClickAction} = props
+    const {cells} = props.game
+    return (
+        <div className="board">
+            {cells.map(cell => {
+                return (
+                    <div
+                        key={cell.id}
+                        onClick={() => cellClickAction(cell.id, cell.text)}
+                        className="cell">{cell.text}
+                    </div>
+                )
+            })}
+        </div>
+    )
 }
+
+export default Board
