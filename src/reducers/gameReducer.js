6d838d56b96636fcd13e8d3255418dbc9b58de0c
Thomas Dittmar <dittmarconsulting@gmail.com>
2017-01-13 11:42:05 +1100

    refactoring reducer logic using functional decomposition

--- src/reducers/gameReducer.js
+++ src/reducers/gameReducer.js
@@ -1,75 +1,84 @@
 import C from '../constants'
 import initState from '../initState'
 
+// Refactoring reducer logic using functional decomposition
+
+const cellCliced = (state, action) => {
+    return {
+        ...state,
+        cells: [
+            // slices the array from beginning to position = action.payload
+            ...state.cells.slice(0, action.payload),
+            // modify the array item of position = action.payload
+            {id: action.payload, text: state.currentPlayer},
+            // add the rest of the array after position = action.payload + 1
+            ...state.cells.slice(action.payload + 1)
+        ]
+    }
+}
+
+const incrementMoveCounter = (state, action) => {
+    return {
+        ...state,
+        currentMove: action.payload
+    }
+}
+
+const swapPlayer = (state, action) => {
+    return {
+        ...state,
+        currentPlayer: action.payload
+    }
+}
+
+const checkWinner = (state, action) => {
+    return {
+        ...state,
+        winnerFound: action.payload
+    }
+}
+
+
+const showDialog = (state, action) => {
+    return {
+        ...state,
+        showDialog: action
+    }
+}
+
+const resetGame = (state) => {
+    return {
+        ...state,
+        cells: initState.cells,
+        currentPlayer: 'X',
+        currentMove: 0,
+        winnerFound: false
+    }
+}
 
 const gameReducer = (state=initState, action) => {
     switch(action.type) {
 
-        // in case someone of a cell click action call
-        case C.CELL_CLICKED : {
-            return {
-                ...state,
-                cells: [
-                    // slices the array from beginning to position = action.payload
-                    ...state.cells.slice(0, action.payload),
-                    // modify the array item of position = action.payload
-                    {id: action.payload, text: state.currentPlayer},
-                    // add the rest of the array after position = action.payload + 1
-                    ...state.cells.slice(action.payload + 1)
-                ]
-            }
-        }
+        // in case of a cell click action call
+        case C.CELL_CLICKED : return cellCliced(state, action)
 
         // in case of a move increment action call
-        case C.INC_MOVE : {
-            return {
-                ...state,
-                currentMove: action.payload
-            }
-        }
+        case C.INC_MOVE : return incrementMoveCounter(state, action)
 
         // in case of a swap player action call
-        case C.SWAP_PLAYER : {
-            return {
-                ...state,
-                currentPlayer: action.payload
-            }
-        }
+        case C.SWAP_PLAYER : return swapPlayer(state, action)
 
         // in case of a check winner action call
-        case C.CHECK_WINNER : {
-            return {
-                ...state,
-                winnerFound: action.payload
-            }
-        }
+        case C.CHECK_WINNER : return checkWinner(state, action)
 
         // in case of an open dialog action call
-        case C.OPEN_DIALOG : {
-            return {
-                ...state,
-                showDialog: true
-            }
-        }
+        case C.OPEN_DIALOG : return showDialog(state, true)
 
         // in case of a close dialog action call
-        case C.CLOSE_DIALOG : {
-            return {
-                ...state,
-                showDialog: false
-            }
-        }
+        case C.CLOSE_DIALOG : return showDialog(state, false)
 
         // in case of a reset game action call
-        case C.RESET_GAME : {
-            return {
-                ...state,
-                cells: initState.cells,
-                currentPlayer: 'X',
-                currentMove: 0,
-                winnerFound: false
-            }
-        }
+        case C.RESET_GAME : return resetGame(state)
 
         default: return state
     }
